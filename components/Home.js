05760cb6813cffbc6905d24e0796f3ed78be87b2
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-05 18:09:31 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,10 @@
 export default Home = () => {
   return (
     <>
-      <div className="head">ao</div>
+      <div className="head">
+        ao
+        <div className="navbar">Home</div>
+      </div>
     </>
   );
 };
