99842d958e2864b936da4c99454395e3d56f2233
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-10 17:54:53 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,46 +1,30 @@
-.App {
-  font-family: sans-serif;
-  text-align: center;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
+import { useState } from "react";
+import ArmorDisplay from "./ArmorDisplay.js";
+import WeaponDisplay from "./WeaponDisplay.js";
 
-.head {
-  width: 90%;
-  height: auto;
-  margin: auto;
-  background-color: lightblue;
-}
-
-.navbar {
-  width: 100px;
-  padding: 10px;
-  background-color: gray;
-  margin-left: auto;
-}
-
-.main {
-  color: yellow;
-  background-color: slateblue;
-  display: flex;
-  flex-grow: 1;
-  width: 90%;
-  margin: auto;
-}
-
-.board {
-  width: 70%;
-}
-
-.side {
-  flex: 2;
-  background-color: blue;
-}
-
-.inventoryNav {
-  margin-left: auto;
-  height: auto;
-  background-color: lightblue;
-  padding: 5vmin;
-}
+export default Home = () => {
+  const [currentView, setCurrentView] = useState("armor");
+  const view = {
+    weapons: <WeaponDisplay />,
+    armor: <ArmorDisplay />,
+  };
+  return (
+    <>
+      <div className="head">
+        ao
+        <div className="navbar">Home</div>
+      </div>
+      <div className="main">
+        <div className="board">board</div>
+        {view[currentView]}
+      </div>
+      <div className="inventory">
+        <div className="inventoryNav">
+          <button onClick={() => setCurrentView("weapons")}>Weapon</button>
+          <button onClick={() => setCurrentView("armor")}>Armor</button>
+          
+        </div>
+      </div>
+    </>
+  );
+};
