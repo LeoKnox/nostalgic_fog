6d236d4a69bf08f49b8d8d6444a2c75297756b65
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-11 18:02:17 -0700

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -4,10 +4,18 @@ import WeaponDisplay from "./WeaponDisplay.js";
 
 export default Home = () => {
   const [currentView, setCurrentView] = useState(null);
-  const [characterPosition, setCharacterPosition] = useState({x:1,y:1});
+  const [characterPosition, setCharacterPosition] = useState({ x: 1, y: 1 });
   const view = {
     weapons: <WeaponDisplay closeOverlay={setCurrentView} />,
     armor: <ArmorDisplay closeOverlay={setCurrentView} />,
+  };
+
+  const keyBoardUse = () => {
+    if (event.key === "ArrowLeft") {
+      [{ ...characterPosition, y: y + 1 }];
+    } else if (event.key === "ArrowRight") {
+      onRight();
+    }
   };
   return (
     <>
@@ -16,14 +24,19 @@ export default Home = () => {
         <div className="navbar">Home</div>
       </div>
       <div className="main">
-        <div className="board"><p>board</p>
-        <p>X:{characterPosition.x} Y:{characterPosition.y}</p></div>
+        <div className="board">
+          <p>board</p>
+          <p>
+            X:{characterPosition.x} Y:{characterPosition.y}
+          </p>
+        </div>
         {view[currentView]}
       </div>
       <div className="inventory">
         <div className="inventoryNav">
           <button onClick={() => setCurrentView("weapons")}>Weapon</button>
           <button onClick={() => setCurrentView("armor")}>Armor</button>
+          <button onClick={() => keyBoardUse()}>move</button>
         </div>
       </div>
     </>
