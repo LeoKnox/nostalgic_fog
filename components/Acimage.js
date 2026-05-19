2a95e626e427cd00f391f632f1a3f0d9aabddb44
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-18 18:09:50 -0700

    Update Acimage.js

--- components/Acimage.js
+++ components/Acimage.js
@@ -1,6 +1,14 @@
-export default = () => {
-    return (
+export default Acimage = () => {
+  return (
     <svg>
-        <circle r="100px" fill="red" />
-    </svg>)
-}
+      <circle
+        r="100"
+        fill="red"
+        cx="100"
+        cy="50"
+        stroke="none"
+        strokeWidth="0"
+      />
+    </svg>
+  );
+};
