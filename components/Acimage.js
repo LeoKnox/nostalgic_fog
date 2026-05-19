1c35876a886f1162cc98929eb6302cf72ba49341
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-19 16:54:45 -0700

    Update Acimage.js

--- components/Acimage.js
+++ components/Acimage.js
@@ -9,6 +9,13 @@ export default Acimage = () => {
         stroke="none"
         strokeWidth="0"
       />
+      <g stroke="black" stroke-width="2">
+        <line x1="50" y1="50" x2="50" y2="10" />
+        <line x1="50" y1="50" x2="85" y2="30" />
+        <line x1="50" y1="50" x2="80" y2="70" />
+        <line x1="50" y1="50" x2="20" y2="70" />
+        <line x1="50" y1="50" x2="15" y2="30" />
+      </g>
     </svg>
   );
 };
