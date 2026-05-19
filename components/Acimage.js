3b08dee291eecb8a01c3d463e536c873896a10c7
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-18 18:57:03 -0700

    Update Acimage.js

--- components/Acimage.js
+++ components/Acimage.js
@@ -4,8 +4,8 @@ export default Acimage = () => {
       <circle
         r="100"
         fill="red"
-        cx="100"
-        cy="50"
+        cx="50%"
+        cy="50%"
         stroke="none"
         strokeWidth="0"
       />
