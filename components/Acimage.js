168c87490b0dfeb43b59cd8781bf86eb53cf315b
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-20 15:56:12 -0700

    Update Acimage.js

--- components/Acimage.js
+++ components/Acimage.js
@@ -11,7 +11,7 @@ export default Acimage = () => {
       />
       <g stroke="black" stroke-width="2">
         <line x1="100" y1="100" x2="100" y2="5" />
-        <line x1="100" y1="100" x2="85" y2={(144 * Math.PI) / 180} />
+        <line x1="100" y1="100" x2={(144 * Math.PI) / 180} y2="70" />
         <line x1="100" y1="100" x2="80" y2="70" />
         <line x1="100" y1="100" x2="20" y2="70" />
         <line x1="100" y1="100" x2="15" y2="30" />
