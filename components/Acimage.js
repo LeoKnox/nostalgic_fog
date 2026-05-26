9109a945ccf8d8c841a10eddcda230f13af0d03d
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-26 14:24:32 -0700

    Add concentric circles and update lines in Acimage

--- components/Acimage.js
+++ components/Acimage.js
@@ -9,12 +9,37 @@ export default Acimage = () => {
         stroke="none"
         strokeWidth="0"
       />
+      <circle
+        r="80"
+        cx="50%"
+        cy="50%"
+        stroke="darkred"
+        strokeWidth="4"
+        fill="none"
+      />
+      <circle
+        r="60"
+        cx="50%"
+        cy="50%"
+        stroke="darkred"
+        strokeWidth="4"
+        fill="none"
+      />
+      <circle
+        r="40"
+        cx="50%"
+        cy="50%"
+        stroke="darkred"
+        strokeWidth="4"
+        fill="none"
+      />
+
       <g stroke="black" stroke-width="2">
-        <line x1="100" y1="100" x2="100" y2="5" />
-        <line x1="100" y1="100" x2={(144 * Math.PI) / 180} y2="70" />
-        <line x1="100" y1="100" x2="80" y2="70" />
-        <line x1="100" y1="100" x2="20" y2="70" />
-        <line x1="100" y1="100" x2="15" y2="30" />
+        <line x1="100" y1="100" x2="100" y2="0" />
+        <line x1="100" y1="100" x2="176" y2="35" />
+        <line x1="100" y1="100" x2="147" y2="181" />
+        <line x1="100" y1="100" x2="53" y2="181" />
+        <line x1="100" y1="100" x2="24" y2="55" />
       </g>
     </svg>
   );
