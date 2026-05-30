321f632340e9587ae1d6e68171ef55012eb9e123
LeoKnox <LeoKnox@users.noreply.github.com>
2026-05-29 17:20:15 -0700

    Create dungeonDB.js

---
+++ components/dungeonDB/dungeonDB.js
@@ -0,0 +1,20 @@
+let dungeonDB = {
+  1: {
+    id: 1,
+    width: 5,
+    length: 5,
+    connectedRooms: [2, 3],
+  },
+  2: {
+    id: 2,
+    width: 6,
+    length: 6,
+    connectedRooms: [1],
+  },
+  3: {
+    id: 3,
+    width: 5,
+    length: 5,
+    connectedRooms: [1],
+  },
+};
