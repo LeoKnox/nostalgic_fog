6bdccbd9186716daf0a10a5fd95ba3beb5647133
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-01 16:31:22 -0700

    Update Room.js

--- components/Room.js
+++ components/Room.js
@@ -1,9 +1,15 @@
-export default Room = () => {
+export default Room = (width = 5, length = 5, height = 3) => {
   const style = {
     viewport: {
       border: "3px solid darkgreen",
       backgroundColor: "gray",
+      width: "500px",
+      height: "400px",
+      perspective: "1200px",
+      perspectiveOrigin: "50% 30%",
+      overflow: "hidden",
     },
+    
   };
   return <div style={style.viewport}>Y</div>;
 };
