e1a79eda4232422475139d370e6474577f692f4c
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-01 16:31:43 -0700

    Add room style for 3D transformation effects

--- components/Room.js
+++ components/Room.js
@@ -9,7 +9,13 @@ export default Room = (width = 5, length = 5, height = 3) => {
       perspectiveOrigin: "50% 30%",
       overflow: "hidden",
     },
-    
+    room: {
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      transformStyle: "preserve-3d",
+      transition: "transform 0.1s ease-out",
+    },
   };
   return <div style={style.viewport}>Y</div>;
 };
