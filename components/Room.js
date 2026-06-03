3a5c9d78513151b29f15086f87a987b554ba9645
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-02 17:45:21 -0700

    Fix className syntax for Room component

--- components/Room.js
+++ components/Room.js
@@ -41,8 +41,8 @@ export default Room = (width = 5, length = 5, height = 3) => {
   return (
     <div style={style.viewport}>
       <div style={style.room}>
-        <div className={`style=${style.face} style=${style.floor}`}></div>
-        <div className={`style=${style.face} style=${style.backWall}`}>Y</div>
+        <div className={`${style.face} ${style.floor}`}></div>
+        <div className={`${style.face} ${style.backWall}`}>Y</div>
       </div>
     </div>
   );
