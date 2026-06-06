5d4e6fe2be39fc1212ce121e08511c029de2c483
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-05 17:13:48 -0700

    Update Room.js

--- components/Room.js
+++ components/Room.js
@@ -33,14 +33,14 @@ export default Room = (width = 5, length = 5, height = 3) => {
       position: "absolute",
       height: "300px",
       width: "200px",
-      transform: "translateZ(900px) rotateX(90deg)",
+      transform: "translateZ(-200px) rotateX(80deg)",
     },
     backWall: {
       backgroundColor: "red",
       position: "absolute",
       width: "200px",
-      height: "200px",
-      transform: "translateZ(00px)",
+      height: "120px",
+      transform: "translateZ(00px) rotateX(10deg)",
       border: "2px solid #ccc",
     },
   };
