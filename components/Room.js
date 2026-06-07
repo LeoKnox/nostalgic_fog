763dfc32e3faff28c2d941cab17663244212cfb9
LeoKnox <LeoKnox@users.noreply.github.com>
2026-06-07 12:45:44 -0700

    Update Room.js

--- components/Room.js
+++ components/Room.js
@@ -1,59 +1,38 @@
-export default Room = (width = 5, length = 5, height = 3) => {
-  const style = {
-    viewport: {
-      border: "3px solid darkgreen",
-      backgroundColor: "gray",
-      width: "600px",
-      height: "400px",
-      perspective: "1200px",
-      perspectiveOrigin: "50% 30%",
-      overflow: "hidden",
-    },
-    room: {
-      position: "relative",
-      width: "100%",
-      height: "100%",
-      transformStyle: "preserve-3d",
-      transition: "transform 0.1s ease-out",
-      perspective: "800px",
-      perspectiveOrigin: "center center",
-      transform:
-        "translateX(10px) rotateX(-40deg) rotateY(20deg) rotateZ(0deg)",
-    },
-    face: {
-      position: "absolute",
-      backgroundColor: "blue",
-      width: "600px",
-      height: "800px",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      backfaceVisibility: "visible",
-    },
-    floor: {
-      backgroundColor: "lightBlue",
-      position: "absolute",
-      height: "300px",
-      width: "200px",
-      transform: "translateX(200px) rotateX(90deg)",
-    },
-    backWall: {
-      backgroundColor: "red",
-      position: "absolute",
-      width: "300px",
-      height: "120px",
-      transform: "translateZ(00px)",
-      border: "2px solid #ccc",
-    },
-  };
+import { N5Kanji } from "./N5Kanji.js";
+
+export default Home = () => {
   return (
-    <div style={style.viewport}>
-      <div style={style.room}>
-        <div>
-          <div style={style.backWall}>Y</div>
-          <div style={style.floor}>X</div>
-        </div>
+    <>
+      <h1>Home</h1>
+      <div>
+        <h2>{N5Kanji[1].Kanji}</h2>
+        <table>
+          <tbody>
+            {N5Kanji.map((row,kanjiId) => (
+              <tr>
+                <td>
+                  <input type="checkbox" value={kanjiId}/>
+                  {kanjiId}
+                </td>
+                {Object.entries(row).map((data) => (
+                  <td
+                    key={data}
+                    style={{
+                      border: "1px solid #ddd",
+                      padding: "8px",
+                      textAlign: "left",
+                      fontWeight: "bold",
+                      fontSize: "1em",
+                    }}
+                  >
+                    {data[0]}:{data[1]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    </div>
+    </>
   );
 };
